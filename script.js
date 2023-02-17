faef7304992ac77ffcbb95683ec48b4a6a96437a
ajceronmiranda <ajceronmiranda@crimson.ua.edu>
2023-02-16 20:37:33 -0600

    new changes

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function apiSearch() {
   };
 
   $.ajax({
-      url: 'my-api-url' + $.param(params),
+      url: 'https://api.bing.microsoft.com/v7.0/search' + $.param(params),
       beforeSend: function (xhrObj) {
-        xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "my-api-key");
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "61df047516d34e268a6bc975ab133913");
       },
       type: "GET",
     })
@@ -28,4 +28,13 @@ function apiSearch() {
     .fail(function () {
       alert("error");
     });
+}
+function Search() {
+    document.getElementById("searchButton").onclick = apiSearch();
+}
+function NewBackground() {
+    document.getElementbyID("title").style = 'background-image: url(https://images.unsplash.com/photo-1534353473418-4cfa6c56fd38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)';
+}
+function ChangeBackground() {
+    document.getElementbyID("title").onclick = NewBackground();
 }
