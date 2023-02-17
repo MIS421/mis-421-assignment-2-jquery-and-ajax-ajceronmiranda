6b70b5e251bb2775fc8f4600c2002d96343d4b76
ajceronmiranda <ajceronmiranda@crimson.ua.edu>
2023-02-16 23:55:13 -0600

    final push

--- WebApplication2/wwwroot/script.js
+++ WebApplication2/wwwroot/script.js
@@ -30,11 +30,20 @@ function apiSearch() {
     });
 }
 function Search() {
-    document.getElementById("searchButton").onclick = apiSearch();
-}
-function NewBackground() {
-    document.getElementbyID("title").style = 'background-image: url(https://images.unsplash.com/photo-1534353473418-4cfa6c56fd38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)';
+    $("#searchButton").click = apiSearch();
 }
 function ChangeBackground() {
-    document.getElementbyID("title").onclick = NewBackground();
+    $("#title").click(function () {
+        style = 'background-image: url(https://images.unsplash.com/photo-1534353473418-4cfa6c56fd38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80)';
+    });
+}
+function GetTime() {
+    var dt = new Date();
+    $("#date-time").innerHTML = dt;
+   
+
+}
+function Time()
+{
+    $("#time").dialog() = GetTime();
 }
